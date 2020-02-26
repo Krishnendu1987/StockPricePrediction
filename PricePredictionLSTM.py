398186b97329dcafcc51af6f61e93047d72296c0
Krishnendu1987 <krishnendu.ke.chowdhury@gmail.com>
2020-02-26 16:22:00 +0530

    New Python Project

--- PricePredictionLSTM.py
+++ PricePredictionLSTM.py
@@ -25,23 +25,51 @@ def predictData(stock, days):
                 filename = os.path.join(os.path.abspath(os.getcwd()), "Exports/", file)
                 stockname = os.path.splitext(os.path.basename(filename))[0]
                 df = pd.read_csv(filename)
-                df1 = pd.read_csv(filename)
-                df1['Date'] = pd.to_datetime(df1.Date, format='%Y-%m-%d')
-                df1.index = df1['Date']
-                data1 = df1.sort_index(ascending=True, axis=0)
-                del df1['Date']
-                X1 = np.array(df1)
-                df['Prediction'] = df['Close'].shift(-1)
-                df.dropna(inplace=True)
+                #df1 = pd.read_csv(filename)
                 df['Date'] = pd.to_datetime(df.Date, format='%Y-%m-%d')
                 df.index = df['Date']
-                data = df.sort_index(ascending=True, axis=0)
+                df = df.sort_index(ascending=True, axis=0)
+
+                df_Test = pd.DataFrame(index=range(0, 15),
+                                       columns=['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
+                j = 0
+                for i in range(len(df) - 15, len(df)):
+                    df_Test['Open'][j] = df['Open'][i]
+                    df_Test['High'][j] = df['High'][i]
+                    df_Test['Low'][j] = df['Low'][i]
+                    df_Test['Close'][j] = df['Close'][i]
+                    df_Test['Adj Close'][j] = df['Adj Close'][i]
+                    df_Test['Volume'][j] = df['Volume'][i]
+
+                    j = j + 1
+                print("Test Set ")
+                #print(df_Test)
+
+                df['Prediction'] = df['Close'].shift(-1)
+                df.dropna(inplace=True)
+
+                # print(df)
                 del df['Date']
+                df_Train = pd.DataFrame(index=range(0, 15),
+                                        columns=['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume', 'Prediction'])
+                j = 0
+                for i in range(len(df) - 15, len(df)):
+                    df_Train['Open'][j] = df['Open'][i]
+                    df_Train['High'][j] = df['High'][i]
+                    df_Train['Low'][j] = df['Low'][i]
+                    df_Train['Close'][j] = df['Close'][i]
+                    df_Train['Adj Close'][j] = df['Adj Close'][i]
+                    df_Train['Volume'][j] = df['Volume'][i]
+                    df_Train['Prediction'][j] = df['Prediction'][i]
+                    j = j + 1
+                print("Train Set ")
+                #print(df_Train)
 
                 forecast_time = int(days)
                 # Predicting the Stock price in the future
-                X = np.array(df.drop(['Prediction'], 1))
-                Y = np.array(df['Prediction'])
+                X_train = np.array(df_Train.drop(['Prediction'], 1))
+                Y_train = np.array(df_Train['Prediction'])
+                X_test = np.array(df_Test)
                 # print("+++++++++ X +++++++++")
                 # print(X)
                 # print("+++++++++ Y +++++++++")
@@ -49,7 +77,8 @@ def predictData(stock, days):
                 #X = preprocessing.scale(X)
                 # converting dataset into x_train and y_train
                 scaler = MinMaxScaler(feature_range=(0, 1))
-                X = scaler.fit_transform(X)
+                X_train = scaler.fit_transform(X_train)
+                X_test = scaler.fit_transform(X_test)
                 #Y = scaler.fit_transform(Y)
                 #X1 = preprocessing.scale(X1)
                 # print("+++++++++ X +++++++++")
@@ -59,7 +88,7 @@ def predictData(stock, days):
                 # print("Forecast : ")
                 # print(X_prediction)
 
-                X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.5)
+                #X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.5)
                 X_train = np.reshape(X_train, ( 1, X_train.shape[0], X_train.shape[1]))
                 X_test = np.reshape(X_test, (1, X_test.shape[0], X_test.shape[1]))
                 print(stockname, " : ", X_train.shape[0], " ", X_train.shape[1], " ", X_train.shape[2])
@@ -67,6 +96,11 @@ def predictData(stock, days):
                 #Y_train = np.reshape(Y_train,(1,Y_train.shape[0]))
                 last_row = np.array([Y_train[-1]])
                 last_row = np.reshape(last_row, (1,last_row.shape[0]))
+                print("Company: ", stockname)
+                print('Last Train Row ')
+                print(last_row)
+                print('Close ')
+                print(np.array([Y_train[-1]]))
                 last_row = scaler.fit_transform(last_row)
                 #print(X_train)
                 #print(Y_train)
@@ -81,6 +115,7 @@ def predictData(stock, days):
                 model.fit(X_train, last_row, epochs=1, batch_size=1, verbose=2)
                 close_price = model.predict(X_test)
                 close_price = scaler.inverse_transform(close_price)
+                print('Prediction')
                 print(close_price)
                 #print(model.summary())
 
